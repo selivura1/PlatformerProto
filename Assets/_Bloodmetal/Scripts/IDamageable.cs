69302a606841236d82338ed08d0cb4f2f4499da1
selivura1 <selivura@list.ru>
2023-11-30 18:14:06 +0500

    Add project files.

---
+++ Assets/_Bloodmetal/Scripts/IDamageable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bloodmetal
+{
+    public interface IDamageable
+    {
+        public void TakeDamage(float amount)
+        {
+
+        }
+        public void Heal(float amount)
+        {
+
+        }
+    }
+}
